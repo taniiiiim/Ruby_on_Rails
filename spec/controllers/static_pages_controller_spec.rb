932ef7f9a97cd87bcb4b92b044548767793e9bd9
vagrant <vagrant@vagrant.com>
2019-09-18 09:21:36 +0000

    Add a full_title helper

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+      assert_select "title", "Ruby on Rails Tutorial Sample App"
 	  end
   end
 
